bbf506525f644a58c4cfd67b8c8a5d97d1d80dea
Clinton Blackburn <clintonb@vori.com>
2023-10-30 22:38:04 -0700

    Added voripos-provision

---
+++ Formula/voripos-provision.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class VoriposProvision < Formula
+  homepage "https://vori.com"
+  url "https://github.com/voriteam/voripos-provision/archive/refs/tags/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "f2c73c9738b0eb12b92d6faf228ff245a9edf638274e1cddd3f650770474acbf"
+
+  depends_on "coreutils" # Needed for base64 decoding
+  depends_on "jq" # Needed for parsing API responses
+  depends_on "voripos-domain-sync"
+  depends_on "voripos-txn-sync"
+
+  def install
+    # Move everything under #{libexec}/
+    libexec.install Dir["*"]
+
+    # Then write executables under #{bin}/
+    bin.write_exec_script (libexec/"voripos-provision.sh")
+  end
+end
