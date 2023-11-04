f307950241d53b9c1fd0d64a499a8f66257783cb
Clinton Blackburn <clintonb@vori.com>
2023-11-04 07:50:13 -0700

    Updated voripos-provision to 0.2.0

--- Formula/voripos-provision.rb
+++ Formula/voripos-provision.rb
@@ -2,9 +2,9 @@
 #                https://rubydoc.brew.sh/Formula
 class VoriposProvision < Formula
   homepage "https://vori.com"
-  url "https://github.com/voriteam/voripos-provision/archive/refs/tags/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "f2c73c9738b0eb12b92d6faf228ff245a9edf638274e1cddd3f650770474acbf"
+  url "https://github.com/voriteam/voripos-provision/archive/refs/tags/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "3ee3bbb72eee4e9c29283438a13d2cad9f18a078522ff06fd20fa16819dae4c7"
 
   depends_on "coreutils" # Needed for base64 decoding
   depends_on "jq" # Needed for parsing API responses
