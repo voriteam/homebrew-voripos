2b8b1959543e403c17f913f0660359c4ca8bff11
Clinton Blackburn <clintonb@vori.com>
2023-11-11 16:02:38 -0800

    Updated voripos-domain-sync to 0.4.2

--- Formula/voripos-domain-sync.rb
+++ Formula/voripos-domain-sync.rb
@@ -1,10 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class VoriposDomainSync < Formula
-  version "0.4.1"
+  version "0.4.2"
   homepage "https://vori.com"
-  url "https://github.com/voriteam/voripos-domain-sync/archive/refs/tags/0.4.1.tar.gz"
-  sha256 "e68958029b50f94693fddc254f5e0d5308146119afb46a4527a57a62277c3095"
+  url "https://github.com/voriteam/voripos-domain-sync/archive/refs/tags/0.4.2.tar.gz"
+  sha256 "dbe1c7583e9f49f7c17b8904df69abc3cd56668fc52aae4ea06eee64433c592f"
 
 
   def install
