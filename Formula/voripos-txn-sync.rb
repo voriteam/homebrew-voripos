9907341cf90b23617538309d2d89096c3b5c962a
Clinton Blackburn <clintonb@vori.com>
2024-05-02 10:27:10 -0700

    Updated voripos-txn-sync to 0.6.0

--- Formula/voripos-txn-sync.rb
+++ Formula/voripos-txn-sync.rb
@@ -2,9 +2,9 @@
 #                https://rubydoc.brew.sh/Formula
 class VoriposTxnSync < Formula
   homepage "https://vori.com"
-  url "https://github.com/voriteam/voripos-txn-sync/archive/refs/tags/0.5.0.tar.gz"
-  version "0.5.0"
-  sha256 "27a37d445d67ffb4ca7deb82a8a76c47ed118e2bd8909e834359ad821b7147d4"
+  url "https://github.com/voriteam/voripos-txn-sync/archive/refs/tags/0.6.0.tar.gz"
+  version "0.6.0"
+  sha256 "6fc256edadf7d673111d27c4ed4b1aa6b5b75df5e02e8a14826facac9519114b"
 
   depends_on "benbjohnson/litestream/litestream"
   depends_on "jq" # Needed for parsing API responses
