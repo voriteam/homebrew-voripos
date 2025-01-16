ffd546913386d382bab9a7638fee81bbfa68b59a
Shohei Miyamoto <shohei@vori.com>
2025-01-15 16:12:20 -0800

    Updated voripos-internet-sharing to 0.1.1 (#12)

--- Formula/voripos-internet-sharing.rb
+++ Formula/voripos-internet-sharing.rb
@@ -2,9 +2,9 @@
 #                https://rubydoc.brew.sh/Formula
 class VoriposInternetSharing < Formula
   homepage "https://vori.com"
-  url "https://github.com/voriteam/vori-pos-internet-sharing-release/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "33e28cef1f05dddc928ff6080d08e9371f083c67cb49cb6621648c1bad00bc4e"
-  version "0.1.0"
+  url "https://github.com/voriteam/vori-pos-internet-sharing-release/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "8ef6ed210de7e107a78a4cd17344413f099d717caa73873a6f7ea53245bd00bb"
+  version "0.1.1"
 
   depends_on "dnsmasq"
 
